f710941ee3d933e574fc9a45a762b183b6b13a25
spicykoala <jeremykelly@bombbomb.com>
2017-07-07 00:46:55 -0600

    - setTimeout should be setInterval. - introduced RESTART_MODE. In this mode, containers will be restarted instead of stopped and removed.

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ Kills unhealthy containers, and reports docker engine health to ELB.
 
 Basic:
 ```bash
-docker run -d -v /var/run/docker.sock:/tmp/docker.sock ecs-watchdog
+docker run -d -v -p 80:80 /var/run/docker.sock:/tmp/docker.sock ecs-watchdog
 ```
 
 Environment variables supported:
 CHECK_INTERVAL=60
 HEALTH_CHECK_PATH=/health-check
 HEALTH_CHECK_PORT=80
-
+RESTART_MODE=0
--- index.js
+++ index.js
@@ -2,18 +2,28 @@ const Docker = require('dockerode');
 
 let docker = new Docker({ socketPath: '/tmp/docker.sock' });
 
-setTimeout(() => {
+const restartMode = !!process.env.RESTART_MODE;
+
+setInterval(() => {
     docker.listContainers({}, (err, containers) => {
        for (let c of containers) {
            if (c.Status.indexOf('(unhealthy)') !== -1) {
                let container = docker.getContainer(c.Id);
-               container.stop().then(() => {
-                   return container.remove();
-               }).then(() => {
-                   console.log(`Successfully stopped unhealthy container: ${c.Image}.`);
-               }).catch(() => {
-                   console.log(`Failed to stop unhealthy container: ${c.Image}.`);
-               })
+               if (restartMode) {
+                   container.restart().then(() => {
+                       console.log(`Successfully restarted unhealthy container: ${c.Image}.`);
+                   }).catch(() => {
+                       console.log(`Failed to restart unhealthy container: ${c.Image}.`);
+                   });
+               } else {
+                   container.stop().then(() => {
+                       return container.remove();
+                   }).then(() => {
+                       console.log(`Successfully stopped unhealthy container: ${c.Image}.`);
+                   }).catch(() => {
+                       console.log(`Failed to stop unhealthy container: ${c.Image}.`);
+                   });
+               }
            }
        }
     });
